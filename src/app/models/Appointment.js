ecc8ffad630267c4d432cd3ff1bbbaa1beef26bb
sergioleno <sergioleno@gmail.com>
2020-02-14 11:08:23 -0300

    GB 8 Verificação de provider

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -18,8 +18,8 @@ class Appointment extends Model {
   static associate(models) {
     // quando uma tabela possui mais de um relacionamento, a chave estrangeira
     // obrigatoriamente precisa de um alias (o as: ...)
-    this.belongsTo(models.Users, { foreignKey: 'user_id', as: 'user' });
-    this.belongsTo(models.Users, { foreignKey: 'provider_id', as: 'provider' });
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -18,10 +19,10 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware); // todas as rotas abaixo utilizarão o middleware
 
-// middleware upload single: arquivo unico, file nome do arquivo
-
 routes.get('/providers', ProviderController.index);
 
+routes.post('/appointments', AppointmentController.store);
+// middleware upload single: arquivo unico, file nome do arquivo
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.put('/users', UserController.update);
